ff1e1bf4c27de4abf038b0695f2be20cf472696d
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-11-15 15:21:48 +0700

    first push

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer (){
+    return(
+        <div className='text-center font-bold text-lg'>
+            All right reserved © by "Sitiporn Wimolpunyakul"
+        </div>
+    );
+}
+export default Footer;
